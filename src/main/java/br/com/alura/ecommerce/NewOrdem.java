b8fd9642c618a98ee34592d88953afa50d09cd86
douglas-nog <douglaswnogueira@hotmail.com>
2023-03-09 07:09:25 -0300

    Criada a serialização com Gson

--- src/main/java/br/com/alura/ecommerce/NewOrdem.java
+++ src/main/java/br/com/alura/ecommerce/NewOrdem.java
@@ -1,23 +1,27 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrdem {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		try (var dispatcher = new KafkaDispatcher()) {
+		try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+			try (var emailDispatcher = new KafkaDispatcher<String>()) {
+				for (var i = 0; i < 10; i++) {
 
-			for (var i = 0; i < 10; i++) {
+					var userId = UUID.randomUUID().toString();
+					var orderId = UUID.randomUUID().toString();
+					var amount = new BigDecimal(Math.random() * 5000 + 1);
+					var order = new Order(userId, orderId, amount);
+					orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-				var key = UUID.randomUUID().toString();
-				var value = key + ", 45654, 147";
-				dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-
-				var email = "Thak you for your order! We are processing your order!";
-				dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+					var email = "Thak you for your order! We are processing your order!";
+					emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+				}
 			}
-		}
 
+		}
 	}
 }
