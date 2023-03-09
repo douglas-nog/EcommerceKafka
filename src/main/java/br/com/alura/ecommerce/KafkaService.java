b1dfae23aaff03615b1fcfe0e7fad7d2973e2c79
douglas-nog <douglaswnogueira@hotmail.com>
2023-03-09 07:28:02 -0300

    Migração do serviço de log

--- src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -16,9 +17,17 @@ class KafkaService implements Closeable {
 	private final ConsumerFunction parse;
 
 	KafkaService(String groupId, String topic, ConsumerFunction parse) {
+		this(parse, groupId);
+		consumer.subscribe(Collections.singletonList(topic));
+	}
+
+	KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+		this(parse, groupId);
+		consumer.subscribe(topic);
+	}
+	private KafkaService(ConsumerFunction parse, String groupId) {
 		this.parse = parse;
 		this.consumer = new KafkaConsumer<>(properties(groupId));
-		consumer.subscribe(Collections.singletonList(topic));
 	}
 
 	void run() {
