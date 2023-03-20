0d2c7d0e171501fca697c2f538376266133e29ce
douglas-nog <douglaswnogueira@hotmail.com>
2023-03-20 17:38:40 -0300

    Remoção do type na classe kafkaService e das classes que utilizam esse serviço

--- ServiceEmail/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ ServiceEmail/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -14,7 +14,6 @@ public class EmailService {
 		try (var service = new KafkaService<>(EmailService.class.getSimpleName(), 
 				"ECOMMERCE_SEND_EMAIL",
 				emailService::parse,
-				String.class,
 				Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
 			service.run();
 		}
